4f0e54fb889a5e21c960528f454d7344df270167
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-03-22 18:49:23 +0700

    update

--- yolox/models/yolox_student.py
+++ yolox/models/yolox_student.py
@@ -56,7 +56,7 @@ class YOLOXStudent(nn.Module):
         # fpn output content features of [dark3, dark4, dark5]
         # print(t_model)
         fpn_outs = self.backbone(x)
-        # print(fpn_outs[0].shape, fpn_outs[1].shape, fpn_outs[2].shape)
+        print(fpn_outs[0].shape, fpn_outs[1].shape, fpn_outs[2].shape)
         if self.training:
             assert targets is not None
             loss, iou_loss, conf_loss, cls_loss, l1_loss, num_fg = self.head(
