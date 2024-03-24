f104cefb55aebd7aa8c64c2629a575e9bf5e3583
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-03-24 20:43:37 +0700

    update

--- yolox/models/yolox_student.py
+++ yolox/models/yolox_student.py
@@ -77,18 +77,8 @@ class YOLOXStudent(nn.Module):
                 t_relation = self.teacher_non_local[i](teacher_feature)
                 # print(s_relation.shape, t_relation.shape)
                 # print(student_feature.shape, teacher_feature.shape)
-                nonlocal_val = torch.dist(self.non_local_adaptation[i](s_relation), t_relation, p=2)
-                foreground_val = torch.dist(self.for_adaptation[i](student_feature), teacher_feature, p=2)
-                if torch.isnan(nonlocal_val):
-                    # print(s_relation)
-                    # print(s_relation)
-                    kd_nonlocal_loss = 20000
-                else:
-                    kd_nonlocal_loss += nonlocal_val
-                if torch.isnan(foreground_val):
-                    kd_foreground_loss = 20000
-                else:
-                    kd_foreground_loss += foreground_val
+                kd_nonlocal_loss += torch.dist(self.non_local_adaptation[i](s_relation), t_relation, p=2)
+                kd_foreground_loss += torch.dist(self.for_adaptation[i](student_feature), teacher_feature, p=2)
 
             kd_nonlocal_loss *= 0.004
             kd_foreground_loss *= 0.006
