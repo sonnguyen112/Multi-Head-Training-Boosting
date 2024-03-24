ae2b8b6ebf00330da8b6155f7b4e38d8465ffab9
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-03-24 14:40:45 +0700

    commit

--- yolox/models/yolox_student.py
+++ yolox/models/yolox_student.py
@@ -66,9 +66,7 @@ class YOLOXStudent(nn.Module):
             kd_nonlocal_loss = 0
             kd_foreground_loss=0
 
-            t_feature = t_model.backbone(x)
-            for i in range(len(t_feature)):
-                t_feature_map[x[i]] = (t_feature[0][i], t_feature[1][i], t_feature[2][i])
+            t_feature_map[x] = t_model.backbone(x)
             # for i in range(3):
             #     student_feature = fpn_outs[i]
             #     teacher_feature = t_model.backbone(x)[i]
