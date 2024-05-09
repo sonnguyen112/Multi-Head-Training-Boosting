654d3db06fe10967d8d0562b84fc119bbb238790
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-05-09 18:58:50 +0700

    update

--- yolox/models/yolox_dual_head.py
+++ yolox/models/yolox_dual_head.py
@@ -51,10 +51,8 @@ class YOLOXDualHead(nn.Module):
             fpn_outs = list(fpn_outs)
             for i in range(len(fpn_outs)):
                 fpn_outs[i] = self.up_sample[i](fpn_outs[i])
-                # fpn_outs[i] = self.reduce_channels[i](fpn_outs[i])
+                fpn_outs[i] = self.reduce_channels[i](fpn_outs[i])
             extra_input = torch.cat(fpn_outs, dim=1)
-            print(extra_input.shape)
-            exit()
             extra_fpn_outs = self.extra_backbone(extra_input)
             extra_loss, extra_iou_loss, extra_conf_loss, extra_cls_loss, extra_l1_loss, extra_num_fg = self.extra_head(
                 extra_fpn_outs, targets, x
