dd1af44eafcde8daf067fde3078072d5b4af40ff
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-05-09 20:05:46 +0700

    commit

--- yolox/models/yolox_dual_head.py
+++ yolox/models/yolox_dual_head.py
@@ -25,16 +25,16 @@ class YOLOXDualHead(nn.Module):
 
         self.backbone = backbone
         self.head = head
-        self.reduce_channels = nn.Sequential(
-            nn.Conv2d(int(256 * self.head.width), 1, kernel_size=1),
-            nn.Conv2d(int(512 * self.head.width), 1, kernel_size=1),
-            nn.Conv2d(int(1024 * self.head.width), 1, kernel_size=1),
-        )
-        self.up_sample = nn.Sequential(
-            nn.Upsample(scale_factor=8, mode="bilinear", align_corners=False),
-            nn.Upsample(scale_factor=16, mode="bilinear", align_corners=False),
-            nn.Upsample(scale_factor=32, mode="bilinear", align_corners=False),
-        )
+        # self.reduce_channels = nn.Sequential(
+        #     nn.Conv2d(int(256 * self.head.width), 1, kernel_size=1),
+        #     nn.Conv2d(int(512 * self.head.width), 1, kernel_size=1),
+        #     nn.Conv2d(int(1024 * self.head.width), 1, kernel_size=1),
+        # )
+        # self.up_sample = nn.Sequential(
+        #     nn.Upsample(scale_factor=8, mode="bilinear", align_corners=False),
+        #     nn.Upsample(scale_factor=16, mode="bilinear", align_corners=False),
+        #     nn.Upsample(scale_factor=32, mode="bilinear", align_corners=False),
+        # )
 
     def forward(self, x, targets=None):
         # fpn output content features of [dark3, dark4, dark5]
@@ -46,12 +46,15 @@ class YOLOXDualHead(nn.Module):
                 fpn_outs, targets, x
             )
 
-            fpn_outs_1 = list(fpn_outs)
-            for i in range(len(fpn_outs_1)):
-                fpn_outs_1[i] = self.up_sample[i](fpn_outs_1[i])
-                fpn_outs_1[i] = self.reduce_channels[i](fpn_outs_1[i])
-            extra_input = torch.cat(fpn_outs_1, dim=1) + x
-            extra_fpn_outs = self.backbone(extra_input)
+            # fpn_outs_1 = list(fpn_outs)
+            # for i in range(len(fpn_outs_1)):
+            #     fpn_outs_1[i] = self.up_sample[i](fpn_outs_1[i])
+            #     fpn_outs_1[i] = self.reduce_channels[i](fpn_outs_1[i])
+            # extra_input = torch.cat(fpn_outs_1, dim=1) + x
+            extra_fpn_outs = self.backbone(x)
+            extra_fpn_outs = list(extra_fpn_outs)
+            for i in range(len(extra_fpn_outs)):
+                extra_fpn_outs[i] += fpn_outs[i]
             extra_loss, extra_iou_loss, extra_conf_loss, extra_cls_loss, extra_l1_loss, extra_num_fg = self.head(
                 extra_fpn_outs, targets, x
             )
