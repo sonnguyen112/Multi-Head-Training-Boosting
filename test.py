712f999547408f90e02b3f8d5dd6ae007a3d2288
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-05-10 17:13:36 +0700

    commit

--- test.py
+++ test.py
@@ -1,24 +1,9 @@
-from PIL import Image
+import torch
+from torch import nn
 
-# Open an image file
-with Image.open('/home/sonnguyen112/Pictures/6045d8bd0572a82cf163.jpg') as img:
-    # Calculate target size maintaining aspect ratio 13:18
-    width = img.size[0]
-    height = img.size[1]
-    aspect_ratio = width / height
-
-    target_aspect_ratio = 13 / 18
-    if aspect_ratio > target_aspect_ratio:
-        # If image is wider than the target aspect ratio, constrain width and adjust height
-        target_width = width
-        target_height = round(target_width / target_aspect_ratio)
-    else:
-        # If image is taller than the target aspect ratio, constrain height and adjust width
-        target_height = height
-        target_width = round(target_height * target_aspect_ratio)
-
-    # Resize the image
-    img_resized = img.resize((target_width, target_height))
-
-# Save the resized image
-img_resized.save('/home/sonnguyen112/Desktop/output.png')
+m = nn.AdaptiveAvgPool2d(1)
+input = torch.randn(1, 64, 8, 9)
+b, c, _, _ = input.size()
+output = m(input).view(b, c)
+print(output.shape)
+print(output)
