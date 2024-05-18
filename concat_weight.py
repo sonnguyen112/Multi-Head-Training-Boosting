445bf4d161b2679e578746d45236a13d055aedcc
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-05-18 20:13:45 +0700

    update

--- concat_weight.py
+++ concat_weight.py
@@ -10,14 +10,27 @@ import torch
 
 #--------------------------------------------------------------------------
 
+# state_dict = torch.load("pretrained/yolox_s.pth")
+# extra_state_dict = torch.load("pretrained/yolox_l.pth")
+# extra_state_dict_1 = torch.load("pretrained/yolox_x.pth")
+# for k in list(extra_state_dict["model"].keys()):
+#     if "head" in k:
+#         state_dict["model"][k.replace("head", "extra_head")] = extra_state_dict["model"][k]
+# for k in list(extra_state_dict_1["model"].keys()):
+#     if "head" in k:
+#         state_dict["model"][k.replace("head", "extra_head_1")] = extra_state_dict_1["model"][k]
+# torch.save(state_dict, "pretrained/yolox_s_with_head_l_head_x.pth.tar")
+# print(state_dict["model"].keys())
+
+#-----------------------------------------------------------------------------
 state_dict = torch.load("pretrained/yolox_s.pth")
-extra_state_dict = torch.load("pretrained/yolox_l.pth")
+extra_state_dict = torch.load("pretrained/yolox_x.pth")
 extra_state_dict_1 = torch.load("pretrained/yolox_x.pth")
 for k in list(extra_state_dict["model"].keys()):
     if "head" in k:
         state_dict["model"][k.replace("head", "extra_head")] = extra_state_dict["model"][k]
 for k in list(extra_state_dict_1["model"].keys()):
     if "head" in k:
-        state_dict["model"][k.replace("head", "extra_head_1")] = extra_state_dict["model"][k]
-torch.save(state_dict, "pretrained/yolox_s_with_head_l_head_x.pth.tar")
+        state_dict["model"][k.replace("head", "extra_head_1")] = extra_state_dict_1["model"][k]
+torch.save(state_dict, "pretrained/yolox_s_with_head_x_head_x.pth.tar")
 print(state_dict["model"].keys())
--- tools/interpolation.py
+++ tools/interpolation.py
@@ -115,9 +115,12 @@ def dti(txt_path, save_path, n_min=25, n_dti=20):
 
 
 if __name__ == '__main__':
-    data_root = '/opt/tiger/demo/ByteTrack/datasets/mot/test'
-    txt_path = '/opt/tiger/demo/ByteTrack/YOLOX_outputs/yolox_x_mix_det/track_results'
-    save_path = '/opt/tiger/demo/ByteTrack/YOLOX_outputs/yolox_x_mix_det/track_results_dti'
+    # data_root = '/opt/tiger/demo/ByteTrack/datasets/mot/test'
+    # txt_path = '/opt/tiger/demo/ByteTrack/YOLOX_outputs/yolox_x_mix_det/track_results'
+    # save_path = '/opt/tiger/demo/ByteTrack/YOLOX_outputs/yolox_x_mix_det/track_results_dti'
+    data_root = '/home/sonnguyen112/Data/HCMUS/Thesis/ByteTrack/datasets/mot/test'
+    txt_path = '/home/sonnguyen112/Data/HCMUS/Thesis/ByteTrack/YOLOX_outputs/yolox_s_with_head_l_head_x_mix_det/track_results'
+    save_path = '/home/sonnguyen112/Data/HCMUS/Thesis/ByteTrack/YOLOX_outputs/yolox_s_with_head_l_head_x_mix_det/track_results_dti'
     
     mkdir_if_missing(save_path)
     dti(txt_path, save_path, n_min=5, n_dti=20)
