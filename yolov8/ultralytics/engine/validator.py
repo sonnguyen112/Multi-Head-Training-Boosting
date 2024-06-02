24bf5ddabc0b4fdc61e70f3548f5506c52a6eff6
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-06-02 15:45:52 +0700

    commit

--- yolov8/ultralytics/engine/validator.py
+++ yolov8/ultralytics/engine/validator.py
@@ -175,7 +175,7 @@ class BaseValidator:
 
             # Inference
             with dt[1]:
-                preds = model(batch["img"], augment=augment)
+                preds = model(batch["img"], augment=augment)[0]
 
             # Loss
             with dt[2]:
