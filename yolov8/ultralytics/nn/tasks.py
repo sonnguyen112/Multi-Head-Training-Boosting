e65d07f2ec4ef6326797d2d33e934daa8dcf44c1
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-06-02 23:56:16 +0700

    commit

--- yolov8/ultralytics/nn/tasks.py
+++ yolov8/ultralytics/nn/tasks.py
@@ -371,7 +371,7 @@ class DetectionModelCustom(BaseModel):
         if isinstance(m, Detect):  # includes all Detect subclasses like Segment, Pose, OBB, WorldDetect
             s = 256  # 2x min stride
             m.inplace = self.inplace
-            forward = lambda x: self.forward(x)[0] if isinstance(m, (Segment, Pose, OBB)) else self.forward(x)[0]
+            forward = lambda x: self.forward(x)[0] if isinstance(m, (Segment, Pose, OBB)) else self.forward(x, is_training = False)
             m.stride = torch.tensor([s / x.shape[-2] for x in forward(torch.zeros(1, ch, s, s))])  # forward
             self.stride = m.stride
             m.bias_init()  # only run once
