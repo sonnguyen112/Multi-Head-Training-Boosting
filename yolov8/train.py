e0a9479b4f535e4efd1b03892cfbeaf0a3f35222
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-06-03 16:34:15 +0700

    commit

--- yolov8/train.py
+++ yolov8/train.py
@@ -4,7 +4,7 @@ from ultralytics import YOLOCustom,YOLO
 # model = YOLO("yolov8n.yaml")
 
 # Load a pretrained YOLO model (recommended for training)
-model = YOLO("yolov8s.yaml").load("yolov8s.pt")
+model = YOLO("yolov8s.yaml").load("yolov8x.pt")
 
 # Train the model using the 'coco8.yaml' dataset for 3 epochs
 results = model.train(data="../datasets/datasets_yolo/mix_det/data.yaml", epochs=80, batch=20, plots=True)
