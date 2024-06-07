e8e1d2efe7a86b081c664c3e22a70844f430f867
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-06-07 13:43:27 +0700

    commit

--- concat_weight.py
+++ concat_weight.py
@@ -25,20 +25,10 @@ import torch
 #-----------------------------------------------------------------------------
 state_dict = torch.load("pretrained/yolox_s.pth")
 extra_state_dict = torch.load("pretrained/yolox_x.pth")
-extra_state_dict_1 = torch.load("pretrained/yolox_x.pth")
-extra_state_dict_2 = torch.load("pretrained/yolox_x.pth")
-extra_state_dict_3 = torch.load("pretrained/yolox_x.pth")
-for k in list(extra_state_dict["model"].keys()):
-    if "head" in k:
-        state_dict["model"][k.replace("head", "extra_head")] = extra_state_dict["model"][k]
-for k in list(extra_state_dict_1["model"].keys()):
-    if "head" in k:
-        state_dict["model"][k.replace("head", "extra_head_1")] = extra_state_dict_1["model"][k]
-for k in list(extra_state_dict_2["model"].keys()):
-    if "head" in k:
-        state_dict["model"][k.replace("head", "extra_head_2")] = extra_state_dict_2["model"][k]
-for k in list(extra_state_dict_3["model"].keys()):
-    if "head" in k:
-        state_dict["model"][k.replace("head", "extra_head_3")] = extra_state_dict_3["model"][k]
-torch.save(state_dict, "pretrained/yolox_s_with_4_head_x.pth.tar")
+num_extra_head = 8
+for i in range(num_extra_head):
+    for k in list(extra_state_dict["model"].keys()):
+        if "head" in k:
+            state_dict["model"][k.replace("head", f"extra_heads.{i}")] = extra_state_dict["model"][k]
+torch.save(state_dict, f"pretrained/yolox_s_with_{num_extra_head}_head_x.pth.tar")
 print(state_dict["model"].keys())
