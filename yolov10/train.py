3b64e8d5ebf76631ab9afd5c10a12449a08cc756
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-06-08 14:14:48 +0700

    commit

--- yolov10/train.py
+++ yolov10/train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLOv10, YOLOv10Custom
 
-model = YOLOv10Custom('yolov10s-custom.pt')
+model = YOLOv10('yolov0x.pt')
 # If you want to finetune the model with pretrained weights, you could load the 
 # pretrained weights like below
 # model = YOLOv10.from_pretrained('jameslahm/yolov10{n/s/m/b/l/x}')
@@ -8,4 +8,4 @@ model = YOLOv10Custom('yolov10s-custom.pt')
 # wget https://github.com/THU-MIG/yolov10/releases/download/v1.1/yolov10{n/s/m/b/l/x}.pt
 # model = YOLOv10('yolov10{n/s/m/b/l/x}.pt')
 
-model.train(data='../datasets/datasets_yolo/mix_det/data.yaml', epochs=100, batch=16, imgsz=640)
+model.train(data='../datasets/datasets_yolo/mix_det/data.yaml', epochs=100, batch=80, imgsz=640)
