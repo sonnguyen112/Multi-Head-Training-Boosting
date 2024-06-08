3583d604fa56c01f6585d0df34144eb63c9d600a
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-06-08 14:15:00 +0700

    commit

--- yolov10/train.py
+++ yolov10/train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLOv10, YOLOv10Custom
 
-model = YOLOv10('yolov0x.pt')
+model = YOLOv10('yolov10x.pt')
 # If you want to finetune the model with pretrained weights, you could load the 
 # pretrained weights like below
 # model = YOLOv10.from_pretrained('jameslahm/yolov10{n/s/m/b/l/x}')
