62d135f3669639bf235556050f76fe9b7b1e6069
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-06-14 22:50:44 +0700

    commit

--- yolox/models/yolox_n_head.py
+++ yolox/models/yolox_n_head.py
@@ -9,6 +9,47 @@ from .yolo_pafpn import YOLOPAFPN
 from .GloRe import GloRe_Unit_2D
 import copy
 import torch
+from torchvision.ops import roi_align
+
+
+class ObjectRelation_1(nn.Module):
+    def __init__(self, in_channels):
+        super(ObjectRelation_1, self).__init__()
+        self.in_channels = in_channels
+        self.inter_channels = in_channels//2
+        self.softmax = nn.Softmax(-1)
+        self.theta = nn.Sequential(
+            nn.Linear(self.in_channels, self.inter_channels))
+        self.phi = nn.Sequential(
+            nn.Linear(self.in_channels, self.inter_channels))
+        self.g = nn.Sequential(
+            nn.Linear(self.in_channels, self.inter_channels))
+        self.W = nn.Linear(self.inter_channels, self.in_channels)
+
+    # def resize(self, x):
+    #     b=x.size(0)
+    #     fc=nn.Linear(self.in_channels, b).cuda()
+    #     mask=fc(x)
+    #     mask=self.softmax(mask)
+    #     return mask
+
+    def forward(self, x):
+        bbox_num = x.size(0)
+        x_r = x.view(bbox_num, -1)
+        g_x = self.g(x_r)  # 2 , 128 , 150 x 150
+        theta_x = self.theta(x_r)  # 2 , 128 , 300 x 300
+        phi_x = self.phi(x_r)  # 2 , 128 , 150 x 150
+        phi_x = phi_x.permute(1, 0)
+        f = torch.matmul(theta_x, phi_x)  # 2 , 300x300 , 150x150
+        # mask=self.resize(x_r)
+        # f = f*mask
+        f_div_C = self.softmax(f)
+
+        y = torch.matmul(f_div_C, g_x)  # 2, 128, 300x300
+        W_y = self.W(y)
+        z = W_y.view(x.size()) + x
+
+        return z
 
 
 class YOLOXMutipleHead(nn.Module):
@@ -38,28 +79,59 @@ class YOLOXMutipleHead(nn.Module):
              nn.Conv2d(int(1024 * self.head.width), int(1024 * e.width), kernel_size=1)]
         ) for e in self.extra_heads])
 
-        self.student_non_local = nn.ModuleList(nn.ModuleList(
+        self.student_non_local = nn.ModuleList(
             [
                 GloRe_Unit_2D(int(256 * extra_head.width), int(256 * extra_head.width)),
                 GloRe_Unit_2D(int(512 * extra_head.width), int(512 * extra_head.width)),
                 GloRe_Unit_2D(int(1024 * extra_head.width), int(1024 * extra_head.width)),
             ]
-        ))
-        self.teacher_non_local = nn.ModuleList(nn.ModuleList(
+        )
+        self.teacher_non_local = nn.ModuleList(
             [
                 GloRe_Unit_2D(int(256 * extra_head.width), int(256 * extra_head.width)),
                 GloRe_Unit_2D(int(512 * extra_head.width), int(512 * extra_head.width)),
                 GloRe_Unit_2D(int(1024 * extra_head.width), int(1024 * extra_head.width)),
             ]
-        ))
-        self.non_local_adaptation = nn.ModuleList(nn.ModuleList([
+        )
+        self.non_local_adaptation = nn.ModuleList([
             nn.Conv2d(int(256 * extra_head.width), int(256 * extra_head.width),
                       kernel_size=1, stride=1, padding=0),
             nn.Conv2d(int(512 * extra_head.width), int(512 * extra_head.width),
                       kernel_size=1, stride=1, padding=0),
             nn.Conv2d(int(1024 * extra_head.width), int(1024 * extra_head.width),
                       kernel_size=1, stride=1, padding=0),
-        ]))
+        ])
+
+        self.student_relation = nn.ModuleList(
+            [
+                ObjectRelation_1(in_channels=128 * 9),
+                ObjectRelation_1(in_channels=256 * 9),
+                ObjectRelation_1(in_channels=512 * 9),
+            ]
+        )
+        self.teacher_relation = nn.ModuleList(
+            [
+                ObjectRelation_1(in_channels=128 * 9),
+                ObjectRelation_1(in_channels=256 * 9),
+                ObjectRelation_1(in_channels=512 * 9),
+            ]
+        )
+        self.relation_adaptation = nn.ModuleList([
+            nn.Conv2d(int(256 * extra_head.width), int(256 * extra_head.width),
+                      kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(int(512 * extra_head.width), int(512 * extra_head.width),
+                      kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(int(1024 * extra_head.width), int(1024 * extra_head.width),
+                      kernel_size=1, stride=1, padding=0),
+        ])
+        self.for_adaptation = nn.ModuleList([
+            nn.Conv2d(int(256 * extra_head.width), int(256 * extra_head.width),
+                      kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(int(512 * extra_head.width), int(512 * extra_head.width),
+                      kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(int(1024 * extra_head.width), int(1024 * extra_head.width),
+                      kernel_size=1, stride=1, padding=0),
+        ])
 
     def forward(self, x, targets=None, t_model=None):
         # fpn output content features of [dark3, dark4, dark5]
@@ -70,6 +142,14 @@ class YOLOXMutipleHead(nn.Module):
             loss, iou_loss, conf_loss, cls_loss, l1_loss, num_fg = self.head(
                 fpn_outs, targets, x
             )
+            mixup = targets.shape[2] > 5
+            if mixup:
+                label_cut = targets[..., :5]
+            else:
+                label_cut = targets
+            nlabel = (label_cut.sum(dim=2) > 0).sum(dim=1)  # number of objects
+            
+                
 
             with torch.no_grad():
                 t_fpn_outs = t_model.backbone(x)
@@ -88,15 +168,41 @@ class YOLOXMutipleHead(nn.Module):
 
                 # Caculate drkd loss
                 kd_nonlocal_loss = 0
+                kd_relation_loss = 0
+                kd_foreground_loss = 0
                 for i in range(len(extra_fpn_outs)):
                     s_relation = self.student_non_local[i](extra_fpn_outs[i])
                     t_relation = self.teacher_non_local[i](t_fpn_outs[i])
-                    kd_nonlocal_loss += torch.dist(self.non_local_adaptation[i](s_relation), t_relation, p=2)
+                    kd_nonlocal_loss += torch.dist(
+                        self.non_local_adaptation[i](s_relation), t_relation, p=2)
+                    
+                    for batch_idx in range(targets.shape[0]):
+                        num_gt = int(nlabel[batch_idx])
+                        gt_bboxes_per_image = targets[batch_idx, :num_gt, 1:5]
+                        # convert from xywh to tlbr
+                        gt_bboxes_per_image[:, :2] = gt_bboxes_per_image[:, :2] - \
+                            gt_bboxes_per_image[:, 2:] / 2
+                        gt_bboxes_per_image[:, 2:] = gt_bboxes_per_image[:, :2] + gt_bboxes_per_image[:, 2:]
+                        # Normalize for all value > 0
+                        gt_bboxes_per_image = torch.clamp(gt_bboxes_per_image, min=0)
+
+                        s_region = roi_align(
+                            extra_fpn_outs[i], boxes=[gt_bboxes_per_image], output_size=3, spatial_scale=extra_fpn_outs[i].shape[-1] / 1088)
+                        t_region = roi_align(
+                            t_fpn_outs[i], boxes=[gt_bboxes_per_image], output_size=3, spatial_scale=t_fpn_outs[i].shape[-1] / 1088)
+                        s_object_relation = self.student_relation[i](s_region)
+                        t_object_relation = self.teacher_relation[i](t_region)
+                        kd_relation_loss += torch.dist(self.relation_adaptation[i](
+                            s_object_relation), t_object_relation, p=2)
+                        kd_foreground_loss += torch.dist(self.for_adaptation[i](s_region), t_region, p=2)
+
+                total_drkd_loss += kd_nonlocal_loss + kd_relation_loss + kd_foreground_loss
 
             outputs = {
-                "total_loss": loss + total_extra_loss,
+                "total_loss": loss + total_extra_loss + total_drkd_loss,
                 "loss": loss,
                 "extra_loss": total_extra_loss,
+                "drkd_loss": total_drkd_loss,
                 "num_extra_head": len(self.extra_heads),
                 "iou_loss": iou_loss,
                 "l1_loss": l1_loss,
