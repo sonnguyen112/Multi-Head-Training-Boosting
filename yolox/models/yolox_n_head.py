9688d85f9839cef12e99cad03641625e4e14d213
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-06-14 22:11:12 +0700

    commit

--- yolox/models/yolox_n_head.py
+++ yolox/models/yolox_n_head.py
@@ -6,7 +6,9 @@ import torch.nn as nn
 
 from .yolo_head import YOLOXHead
 from .yolo_pafpn import YOLOPAFPN
+from .GloRe import GloRe_Unit_2D
 import copy
+import torch
 
 
 class YOLOXMutipleHead(nn.Module):
@@ -27,15 +29,39 @@ class YOLOXMutipleHead(nn.Module):
 
         self.backbone = backbone
         self.head = head
-        self.extra_heads = nn.Sequential(*[copy.deepcopy(extra_head) for _ in range(num_extra_head)])
-                                         
-        self.up_channels = nn.Sequential(*[nn.Sequential(
-            nn.Conv2d(int(256 * self.head.width), int(256 * e.width), kernel_size=1),
-            nn.Conv2d(int(512 * self.head.width), int(512 * e.width), kernel_size=1),
-            nn.Conv2d(int(1024 * self.head.width), int(1024 * e.width), kernel_size=1),
+        self.extra_heads = nn.ModuleList([copy.deepcopy(extra_head)
+                                         for _ in range(num_extra_head)])
+
+        self.up_channels = nn.ModuleList([nn.ModuleList(
+            [nn.Conv2d(int(256 * self.head.width), int(256 * e.width), kernel_size=1),
+             nn.Conv2d(int(512 * self.head.width), int(512 * e.width), kernel_size=1),
+             nn.Conv2d(int(1024 * self.head.width), int(1024 * e.width), kernel_size=1)]
         ) for e in self.extra_heads])
 
-    def forward(self, x, targets=None):
+        self.student_non_local = nn.ModuleList([nn.ModuleList(
+            [
+                GloRe_Unit_2D(int(256 * e.width), int(256 * e.width)),
+                GloRe_Unit_2D(int(512 * e.width), int(512 * e.width)),
+                GloRe_Unit_2D(int(1024 * e.width), int(1024 * e.width)),
+            ]
+        ) for e in self.extra_heads])
+        self.teacher_non_local = nn.ModuleList([nn.ModuleList(
+            [
+                GloRe_Unit_2D(int(256 * e.width), int(256 * e.width)),
+                GloRe_Unit_2D(int(512 * e.width), int(512 * e.width)),
+                GloRe_Unit_2D(int(1024 * e.width), int(1024 * e.width)),
+            ]
+        ) for e in self.extra_heads])
+        self.non_local_adaptation = nn.ModuleList([nn.ModuleList([
+            nn.Conv2d(int(256 * e.width), int(256 * e.width),
+                      kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(int(512 * e.width), int(512 * e.width),
+                      kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(int(1024 * e.width), int(1024 * e.width),
+                      kernel_size=1, stride=1, padding=0),
+        ]) for e in self.extra_heads])
+
+    def forward(self, x, targets=None, t_model=None):
         # fpn output content features of [dark3, dark4, dark5]
         fpn_outs = self.backbone(x)
 
@@ -45,7 +71,11 @@ class YOLOXMutipleHead(nn.Module):
                 fpn_outs, targets, x
             )
 
-            extra_losses = []
+            with torch.no_grad():
+                t_fpn_outs = t_model.backbone(x)
+
+            total_extra_loss = 0
+            total_drkd_loss = 0
             for extra_index, extra_head in enumerate(self.extra_heads):
                 extra_fpn_outs = list(fpn_outs)
                 for i in range(len(extra_fpn_outs)):
@@ -54,18 +84,20 @@ class YOLOXMutipleHead(nn.Module):
                 extra_loss, extra_iou_loss, extra_conf_loss, extra_cls_loss, extra_l1_loss, extra_num_fg = self.extra_heads[extra_index](
                     extra_fpn_outs, targets, x
                 )
-                extra_losses.append(extra_loss)
+                total_extra_loss += extra_loss
 
-            total_loss = loss
-
-            for extra_loss in extra_losses:
-                total_loss += extra_loss
+                # Caculate drkd loss
+                kd_nonlocal_loss = 0
+                for i in range(len(extra_fpn_outs)):
+                    s_relation = self.student_non_local[extra_index][i](extra_fpn_outs[i])
+                    t_relation = self.teacher_non_local[extra_index][i](t_fpn_outs[i])
+                    kd_nonlocal_loss += torch.dist(self.non_local_adaptation[extra_index][i](s_relation), t_relation, p=2)
 
             outputs = {
-                "total_loss": total_loss,
+                "total_loss": loss + total_extra_loss,
                 "loss": loss,
-                "extra_loss_min": min(extra_losses),
-                "num_extra_head": len(extra_losses),
+                "extra_loss": total_extra_loss,
+                "num_extra_head": len(self.extra_heads),
                 "iou_loss": iou_loss,
                 "l1_loss": l1_loss,
                 "conf_loss": conf_loss,
