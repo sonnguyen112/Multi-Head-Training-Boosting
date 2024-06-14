4b97faeef5d9c576a2bd0baa8db9b6ff9a276336
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-06-14 22:32:22 +0700

    commit

--- yolox/models/yolox_n_head.py
+++ yolox/models/yolox_n_head.py
@@ -38,28 +38,28 @@ class YOLOXMutipleHead(nn.Module):
              nn.Conv2d(int(1024 * self.head.width), int(1024 * e.width), kernel_size=1)]
         ) for e in self.extra_heads])
 
-        self.student_non_local = nn.ModuleList([nn.ModuleList(
+        self.student_non_local = nn.ModuleList(nn.ModuleList(
             [
-                GloRe_Unit_2D(int(256 * e.width), int(256 * e.width)),
-                GloRe_Unit_2D(int(512 * e.width), int(512 * e.width)),
-                GloRe_Unit_2D(int(1024 * e.width), int(1024 * e.width)),
+                GloRe_Unit_2D(int(256 * extra_head.width), int(256 * extra_head.width)),
+                GloRe_Unit_2D(int(512 * extra_head.width), int(512 * extra_head.width)),
+                GloRe_Unit_2D(int(1024 * extra_head.width), int(1024 * extra_head.width)),
             ]
-        ) for e in self.extra_heads])
-        self.teacher_non_local = nn.ModuleList([nn.ModuleList(
+        ))
+        self.teacher_non_local = nn.ModuleList(nn.ModuleList(
             [
-                GloRe_Unit_2D(int(256 * e.width), int(256 * e.width)),
-                GloRe_Unit_2D(int(512 * e.width), int(512 * e.width)),
-                GloRe_Unit_2D(int(1024 * e.width), int(1024 * e.width)),
+                GloRe_Unit_2D(int(256 * extra_head.width), int(256 * extra_head.width)),
+                GloRe_Unit_2D(int(512 * extra_head.width), int(512 * extra_head.width)),
+                GloRe_Unit_2D(int(1024 * extra_head.width), int(1024 * extra_head.width)),
             ]
-        ) for e in self.extra_heads])
-        self.non_local_adaptation = nn.ModuleList([nn.ModuleList([
-            nn.Conv2d(int(256 * e.width), int(256 * e.width),
+        ))
+        self.non_local_adaptation = nn.ModuleList(nn.ModuleList([
+            nn.Conv2d(int(256 * extra_head.width), int(256 * extra_head.width),
                       kernel_size=1, stride=1, padding=0),
-            nn.Conv2d(int(512 * e.width), int(512 * e.width),
+            nn.Conv2d(int(512 * extra_head.width), int(512 * extra_head.width),
                       kernel_size=1, stride=1, padding=0),
-            nn.Conv2d(int(1024 * e.width), int(1024 * e.width),
+            nn.Conv2d(int(1024 * extra_head.width), int(1024 * extra_head.width),
                       kernel_size=1, stride=1, padding=0),
-        ]) for e in self.extra_heads])
+        ]))
 
     def forward(self, x, targets=None, t_model=None):
         # fpn output content features of [dark3, dark4, dark5]
@@ -89,9 +89,9 @@ class YOLOXMutipleHead(nn.Module):
                 # Caculate drkd loss
                 kd_nonlocal_loss = 0
                 for i in range(len(extra_fpn_outs)):
-                    s_relation = self.student_non_local[extra_index][i](extra_fpn_outs[i])
-                    t_relation = self.teacher_non_local[extra_index][i](t_fpn_outs[i])
-                    kd_nonlocal_loss += torch.dist(self.non_local_adaptation[extra_index][i](s_relation), t_relation, p=2)
+                    s_relation = self.student_non_local[i](extra_fpn_outs[i])
+                    t_relation = self.teacher_non_local[i](t_fpn_outs[i])
+                    kd_nonlocal_loss += torch.dist(self.non_local_adaptation[i](s_relation), t_relation, p=2)
 
             outputs = {
                 "total_loss": loss + total_extra_loss,
