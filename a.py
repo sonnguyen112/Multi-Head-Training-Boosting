8c3f83b45dace7dba847f18273d1d590e046fa88
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-06-17 19:58:00 +0700

    commit

--- a.py
+++ a.py
@@ -5,3 +5,4 @@ b = torch.tensor([1, 2]).to(dtype=torch.float)
 
 cosine = torch.nn.CosineSimilarity(dim=0, eps=1e-6)
 print(cosine(a, b))
+print(1 - cosine(a,b))
--- yolox/models/yolox_n_head.py
+++ yolox/models/yolox_n_head.py
@@ -159,6 +159,7 @@ class YOLOXMutipleHead(nn.Module):
                     flatten_fpn_outs = extra_fpn_outs[i].view(extra_fpn_outs[i].shape[0], -1)
                     flatten_t_fpn_outs = t_fpn_outs[i].view(t_fpn_outs[i].shape[0], -1)
                     cosine_sim = self.cosine_similarity(flatten_fpn_outs, flatten_t_fpn_outs)
+                    print(cosine_sim)
                     print(1 - cosine_sim)
                     exit()
                 extra_fpn_outs = tuple(extra_fpn_outs)
