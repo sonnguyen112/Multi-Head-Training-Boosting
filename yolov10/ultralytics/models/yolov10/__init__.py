c35fffbd292b91e47d748fbca92b08bb9451cd93
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2024-10-22 13:48:05 +0700

    commit

--- yolov10/ultralytics/models/yolov10/__init__.py
+++
@@ -1,5 +0,0 @@
-from .model import YOLOv10, YOLOv10Custom
-from .predict import YOLOv10DetectionPredictor
-from .val import YOLOv10DetectionValidator
-
-__all__ = "YOLOv10DetectionPredictor", "YOLOv10DetectionValidator", "YOLOv10", "YOLOv10Custom"
